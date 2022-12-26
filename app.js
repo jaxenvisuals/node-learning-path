75ecb2557422c02558765caed1c7868cc0b6a231
jaxenvisuals <jaxenvisuals@gmail.com>
2022-12-26 12:06:15 +0000

    Parsing data from client

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-  console.log(url);
 
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
-    res.write("<h1>Hello World</h1>");
+    res.write(`
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1>Home</h1>
+            <form method="post" action="/about">
+                <input type="text" name="testVariable">
+                <input type="submit">
+            </form>
+        </body>
+    </html>
+      `);
     return res.end();
   } else if (url === "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<h1>About</h1>");
-    return res.end();
+    if (req.method === "POST") {
+      const message = [];
+
+      req.on("data", (packet) => {
+        message.push(packet);
+      });
+
+      req.on("end", () => {
+        const parsed = Buffer.concat(message).toString();
+
+        fs.writeFileSync("message.txt", parsed.split("=")[1]);
+
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h1>About Posted</h1>");
+        return res.end();
+      });
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.write("<h1>About</h1>");
+      return res.end();
+    }
   } else {
     res.setHeader("Content-Type", "text/html");
     res.write("<h1>404</h1>");
