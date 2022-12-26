23dfe3e00b145e4ee9d53a205cc8a0121b4764f5
jaxenvisuals <jaxenvisuals@gmail.com>
2022-12-26 11:49:11 +0000

    Routing

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req, res);
+  const url = req.url;
+  console.log(url);
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Hello World</h1>");
+    return res.end();
+  } else if (url === "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>About</h1>");
+    return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>404</h1>");
+    return res.end();
+  }
 });
 
 server.listen(3000);
