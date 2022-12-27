61108ff3e5d8935405da1c874b2296588ff3d692
jaxenvisuals <jaxenvisuals@gmail.com>
2022-12-27 22:32:21 +0000

    Serving static files

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const router = require("./routes/index");
+const rootDir = require("./util/path");
 
 const app = express();
+
+// Serve static files
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
