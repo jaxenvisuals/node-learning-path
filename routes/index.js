3bdf6b5a6685613839c8fe0d560ebd437401f966
jaxenvisuals <jaxenvisuals@gmail.com>
2022-12-27 21:50:16 +0000

    Using path module import for navigation

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const products = require("./products");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // Home
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, "../", "views", "home.html"));
+  res.status(200).sendFile(path.join(rootDir, "views", "home.html"));
 });
 
 router.use("/products", products);
@@ -18,9 +18,7 @@ router.use("/products", products);
 // Catch all unresolved urls
 router.use((req, res, next) => {
   // 404 page
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, "../", "views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 module.exports = router;
