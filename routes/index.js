5911f51f7005a0e863033d1bc7776ca165df7f9c
jaxenvisuals <jaxenvisuals@gmail.com>
2022-12-27 21:24:12 +0000

    Serving HTML files

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const products = require("./products");
@@ -6,20 +8,9 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // Home
-  res.status(200).send(`
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Shop</title>
-        </head>
-        <body>
-            <h1>Welcome to Shop!</h1>
-            <p>Buy products here</p>
-        </body>
-    </html>
-      `);
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, "../", "views", "home.html"));
 });
 
 router.use("/products", products);
@@ -27,20 +18,9 @@ router.use("/products", products);
 // Catch all unresolved urls
 router.use((req, res, next) => {
   // 404 page
-  res.status(404).send(`
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <h1>404 Page</h1>
-            <p>Route: ${req.url} was not found on the server</p>
-        </body>
-    </html>
-      `);
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "../", "views", "404.html"));
 });
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,24 +1,13 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
-console.log("here");
-
 router.get("/", (req, res, next) => {
-  res.status(200).send(`
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <h1>Products</h1>
-            <p>Here's the product page</p>
-        </body>
-    </html>
-      `);
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, "../", "views", "products.html"));
 });
 
 module.exports = router;
